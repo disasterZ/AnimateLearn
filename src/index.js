86d99967a3f71498f46b925b8b93c5250e73565f
disasterZ <937934696@qq.com>
2020-10-26 14:41:50 +0800

    react-transition-group

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './TodoList.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <TodoList />,
   document.getElementById('root')
 );
 
